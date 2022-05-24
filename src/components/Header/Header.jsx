f3972b77eef125c3de544bffcd81ef23701b2943
Nikita <nikitkakhmelevskiy1@gmail.com>
2022-05-24 14:27:05 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,11 @@ import './index.scss';
 export default function Header() {
   	return (
 		<div className="container">
-			
+			<div className="row">
+				<div className="logo">
+					<img src="" alt="#" />
+				</div>
+			</div>
 		</div>
   	)	
 }
